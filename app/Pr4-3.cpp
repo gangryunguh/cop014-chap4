8ebc81ffce40185fe56e94505748609e0daee926
Gang-Ryung Uh <gangryunguh@gmail.com>
2018-05-27 17:15:46 -0500

    Update CMakeLists.txt to build individual sample programs.

--- app/Pr4-3.cpp
+++ app/Pr4-3.cpp
@@ -8,7 +8,7 @@ int main()
    int x = 0, y = 10;
 
    cout << "x is " << x << " and y is " << y << endl;
-   if (x > y);    // Error! Misplaced semicolon
+   if (x > y)    // Error! Misplaced semicolon
       cout << "x is greater than y\n"; //This is always executed.
    return 0;
 }
